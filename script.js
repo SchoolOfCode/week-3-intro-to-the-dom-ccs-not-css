ca5645d6c6bd28afc5ad4fc7a531140cb0f360c2
Christopher O'Connor <christopher.oconnor1@hotmail.com>
2024-12-09 13:54:15 +0000

    Ticket 1b

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-
+// Ticket 1a
 const title = document.getElementById("title");
-
 console.log(title);
+
+// Ticket 1b
+const content = document.getElementsByClassName("content");
+console.log(content[1]);
